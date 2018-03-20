4eb7ebfd775d510f70c007d076e6ceb8f7a0815e
Victor Luis <victorluis@devaid.cl>
2018-03-20 12:32:04 -0300

    updates

--- app.js
+++ app.js
@@ -16,63 +16,63 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 
 /*app.use(session({
-	secret: "123jhhahah",
-	resave: false,
-	saveUninitialized: false
+    secret: "123jhhahah",
+    resave: false,
+    saveUninitialized: false
 }))*/
 
 app.use(cookieSession({
-	name: "session",
-	keys: ["llave-1","llave-2"]
+    name: "session",
+    keys: ["llave-1","llave-2"]
 }));
 
 app.use(formidable.parse({ keepExtensions: true }));
 app.set("view engine", "jade");
 
 app.get("/",function(req,res){
-	//console.log(req.session.user_id);
-	res.render("login");
+    //console.log(req.session.user_id);
+    res.render("login");
 });
 
 app.get("/signup",function(req,res){
-	User.find(function(err,doc){
-		console.log(doc);
-		res.render("signup");
-	});
+    User.find(function(err,doc){
+        console.log(doc);
+        res.render("signup");
+    });
 });
 
 app.get("/login",function(req,res){
-	res.render("login");
+    res.render("login");
 });
 
 app.post("/users",function(req,res){
-	var user = new User({email: req.body.email, 
-						password: req.body.password,
-						password_confirmation: req.body.password_confirmation,
-						username: req.body.username
-						});
-	user.save().then(function(us){
-		res.send("Guardamos el usuario exitosamente");
-		res.redirect("/login");
-	},function(err){
-		if(err){
-			console.log(String(err));
-			res.send("No Pudimos guardar la información");
-		}
-	});
+    var user = new User({email: req.body.email, 
+                        password: req.body.password,
+                        password_confirmation: req.body.password_confirmation,
+                        username: req.body.username
+                        });
+    user.save().then(function(us){
+        res.send("Guardamos el usuario exitosamente");
+        res.redirect("/login");
+    },function(err){
+        if(err){
+            console.log(String(err));
+            res.send("No Pudimos guardar la información");
+        }
+    });
 });
 
 app.post("/sessions", function(req,res){
-	User.findOne({username: req.body.username, password: req.body.password}, function(err,user){
-		if(err || !user){
-			console.log(String(err));
-			res.send("Usuario y/o Contraseña Inválidos");
-		}
-		else{
-			req.session.user_id = user._id;
-			res.redirect("/app");
-		}
-	});
+    User.findOne({username: req.body.username, password: req.body.password}, function(err,user){
+        if(err || !user){
+            console.log(String(err));
+            res.send("Usuario y/o Contraseña Inválidos");
+        }
+        else{
+            req.session.user_id = user._id;
+            res.redirect("/app");
+        }
+    });
 });
 
 app.use("/app", session_middleware);
